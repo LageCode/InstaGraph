0d32a8dc3de1a704b241aef7b7009d1a31950d47
Léo Calvo <leocalvocast@outlook.be>
2022-09-28 08:52:20 +0200

    a

--- instagraph/app/views.py
+++ instagraph/app/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+# Create your views here.
 
 def index(request):
-    return HttpResponse("HW!")
-
-# Create your views here.
+    BASE_URL = "https://api.instagram.com/oauth/authorize/?"
+    REDIRECT_URI = "http://127.0.0.1:5000/grant-access"
+    url = BASE_URL + "client_id={}&redirect_uri={}&response_type=code".format(CLIENT_ID, REDIRECT_URI)
+    return HttpResponseRedirect(url)
+    #return render(request, "index.html")
